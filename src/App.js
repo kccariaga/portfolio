7dfcda7820a3859cea7db7883efe3bbb2be5b2d1
kccariaga <kccariaga@ucsc.edu>
2018-10-09 13:29:55 -0700

    basic header and body layout, no content added yet

--- src/App.js
+++ src/App.js
@@ -6,10 +6,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      
+        <div class="container-fullwidth center">
+          <div className="navbar">
+         
+            <ul class="nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="#!">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://github.com/kccariaga">Github</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/in/karen-cariaga-2a8241123/">LinkedIn</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
         <header className="App-header">
-          <p>Hi I'm Karen.</p>
-        
+
+          <div class = "self-portrait">
+          <img src="https://media.tenor.com/images/cbff5f3a76b4da86ea39120496c6af7b/tenor.gif" class = "rounded center"></img>
+          </div>
+          <h1>Karen Cariaga</h1>
+          <h2>Programmer</h2>
+          <h3>Seeking new opportunities</h3>
         </header>
+
+
+        <body className = "App-body">
+
+          <p>
+            Thank you for stumbling across my webpage. I am a soon-to-be Computer Science
+            graduate of the beautiful campus, UC Santa Cruz. Scroll down below to learn more about
+            me and the skills I have to offer.
+            </p>
+        </body>
+
+
       </div>
     );
   }
