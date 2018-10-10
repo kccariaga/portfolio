5caf6da06c07485a713b521309d194c036565899
kccariaga <kccariaga@ucsc.edu>
2018-10-09 20:02:32 -0700

    container grid setup

--- src/App.js
+++ src/App.js
@@ -24,25 +24,32 @@ class App extends Component {
           </div>
         </div>
 
-        <header className="App-header">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <header className="App-header">
 
-          <div class = "self-portrait">
-          <img src="https://media.tenor.com/images/cbff5f3a76b4da86ea39120496c6af7b/tenor.gif" class = "rounded center"></img>
+                <div class="self-portrait">
+                  <img src="https://media.tenor.com/images/cbff5f3a76b4da86ea39120496c6af7b/tenor.gif" class="rounded center"></img>
+                
+                  <h1>Karen Cariaga</h1>
+                  <h4>Programmer</h4>
+                  <h6>Seeking new opportunities</h6>
+                </div>
+              </header>
+           </div>
+            <div class="col-md-8">
+              <body className="App-body">
+
+                <p>
+                  Thank you for stumbling across my webpage. I am a soon-to-be Computer Science
+                  graduate of the beautiful campus, UC Santa Cruz. Scroll down below to learn more about
+                  me and the skills I have to offer.
+              </p>
+              </body>
+           </div>
           </div>
-          <h1>Karen Cariaga</h1>
-          <h2>Programmer</h2>
-          <h3>Seeking new opportunities</h3>
-        </header>
-
-
-        <body className = "App-body">
-
-          <p>
-            Thank you for stumbling across my webpage. I am a soon-to-be Computer Science
-            graduate of the beautiful campus, UC Santa Cruz. Scroll down below to learn more about
-            me and the skills I have to offer.
-            </p>
-        </body>
+        </div>
 
 
       </div>
