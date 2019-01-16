2e8a380e222823bf09cbfdf91ac1ad2d3af97879
kccariaga <kccariaga@ucsc.edu>
2019-01-16 05:42:27 -0800

    fixed

--- src/index.js
+++ src/index.js
@@ -11,39 +11,6 @@ import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom';
 
-import { render } from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { applyMiddleware, compose, createStore } from 'redux';
-import createHistory from 'history/createHashHistory';
-import { routerMiddleware, ConnectedRouter } from 'react-router-redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-
-import rootReducer from './reducers';
-
-const history = createHistory();
-injectTapEventPlugin();
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  rootReducer,
-  composeEnhancer(
-    applyMiddleware(
-      routerMiddleware(history),
-      thunk
-    ),
-  ),
-);
-
-render(
-  <MuiThemeProvider>
-    <Provider store={store}>
-      <App history={history} />
-    </Provider>
-  </MuiThemeProvider>,
-  document.getElementById('root'));
-
-
 ReactDOM.render(
   <BrowserRouter>
     <App />
