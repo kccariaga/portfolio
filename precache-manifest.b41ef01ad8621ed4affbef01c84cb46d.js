ca39388a4c40d2c2c0e7432ff4a675dc38e62886
kccariaga <kccariaga@ucsc.edu>
2019-01-17 00:40:20 -0800

    Updates

--- precache-manifest.b41ef01ad8621ed4affbef01c84cb46d.js
+++ precache-manifest.b41ef01ad8621ed4affbef01c84cb46d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "39785f36753a680c7a46",
-    "url": "/portfolio/static/js/main.39785f36.chunk.js"
+    "revision": "7060b16975237ee798d8",
+    "url": "/portfolio/static/js/main.7060b169.chunk.js"
   },
   {
     "revision": "5347862e1d761ebab040",
     "url": "/portfolio/static/js/1.5347862e.chunk.js"
   },
   {
-    "revision": "39785f36753a680c7a46",
+    "revision": "7060b16975237ee798d8",
     "url": "/portfolio/static/css/main.b4ab4b51.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/1.a45a72a4.chunk.css"
   },
   {
-    "revision": "761507d9e80ebca253d2fe7d36c07499",
+    "revision": "a8f290995d0e5cd32ff4f7af042f98d7",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.19165df288971a26231c9a52ba5096ac.js"
+  "/portfolio/precache-manifest.b41ef01ad8621ed4affbef01c84cb46d.js"
 );
 
 workbox.clientsClaim();
