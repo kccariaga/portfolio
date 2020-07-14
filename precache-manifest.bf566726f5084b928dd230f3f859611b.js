ccffc5b557e74a79b8f0813de08bc3d19255dfdf
kccariaga <karenc.cariaga@gmail.com>
2020-07-13 20:43:32 -0700

    Updates

---
+++ precache-manifest.bf566726f5084b928dd230f3f859611b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae800182ab86928dde1f",
+    "url": "/portfolio/static/css/main.8ba032c9.chunk.css"
+  },
+  {
+    "revision": "ae800182ab86928dde1f",
+    "url": "/portfolio/static/js/main.36a8bc7c.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "37afb22b301ed556c50f",
+    "url": "/portfolio/static/css/2.9520c862.chunk.css"
+  },
+  {
+    "revision": "37afb22b301ed556c50f",
+    "url": "/portfolio/static/js/2.da5c04f1.chunk.js"
+  },
+  {
+    "revision": "587dc3d8459669da477624c02ae9ed34",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.88f9beeb109060a322a3c7695bcd6a95.js"
+  "/portfolio/precache-manifest.bf566726f5084b928dd230f3f859611b.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
