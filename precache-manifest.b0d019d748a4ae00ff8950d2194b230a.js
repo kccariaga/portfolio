51497b449e6268d30355e7ecef8be3c361225c91
kccariaga <karenc.cariaga@gmail.com>
2020-08-03 20:43:25 -0700

    Updates

---
+++ precache-manifest.b0d019d748a4ae00ff8950d2194b230a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5504a99572615df3056197dbb08632b7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8d6429dc63a325261201",
+    "url": "/portfolio/static/css/2.d4b8c9c4.chunk.css"
+  },
+  {
+    "revision": "7240c95e3129efd79d79",
+    "url": "/portfolio/static/css/main.8768870d.chunk.css"
+  },
+  {
+    "revision": "8d6429dc63a325261201",
+    "url": "/portfolio/static/js/2.8c60ff20.chunk.js"
+  },
+  {
+    "revision": "1b645e95e5b8cfba6569bf6ae9a227c3",
+    "url": "/portfolio/static/js/2.8c60ff20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7240c95e3129efd79d79",
+    "url": "/portfolio/static/js/main.b63db592.chunk.js"
+  },
+  {
+    "revision": "bf088f763ddea8ba7cf2",
+    "url": "/portfolio/static/js/runtime-main.46dea64d.js"
+  }
+]);
