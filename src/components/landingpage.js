a2ea78a48ff5cfbee27a373aaecd52fc6e6db82a
kccariaga <karenc.cariaga@gmail.com>
2023-05-18 00:46:45 -0700

    Update Resume and landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, } from 'react-mdl';
+
 
 
 class LandingPage extends Component {
   render() {
     return (
-      <div style ={{width: '100%', margin:'auto'}}>
+      <div style ={{width: '100%', margin:'auto', }}>
         <Grid className = "landing-grid">
-          <Cell col={12}>
+          <Cell col={3}></Cell>
+          <Cell col={6}>
             <img
               src="/portfolio/images/profilepic4.jpg"
               alt="avatar"
               className="avatar-img"
             />
 
-            <div className="banner-text">
-              {/* <h1>Software Engineer</h1>
-              <hr/> */}
+            <div className="banner-text">  
               <h2>Hello World.</h2>
-              <p> My name is Karen and coding is my passion. Thank you for stumbling upon my personal portfolio. Based in Sacramento, I strive to find new ways 
+              <p> My name is Karen and I'm a Software Engineer based in the Greater Sacramento Area. Thank you for stumbling upon my personal portfolio. I strive to find new ways 
                 to refine my skills through personal projects and opportunities in my professional work.  </p>
-               <p> Java | C | C++ | Python | SQL | Javascript | HTML | CSS | React | Node </p> 
+
+
 
               <div className="social-links">
                 {/* target:"_blank" ensures that your website stays open on a tab when they click your link */}
@@ -34,9 +35,13 @@ class LandingPage extends Component {
                 <a href="https://github.com/kccariaga" rel="noopener noreferrer" target="_blank">
                   <i className="fa fa-github-square" aria-hidden="true" />
                 </a>
+
+
               </div>
             </div>
           </Cell>
+
+          <Cell col={3}></Cell>
 
         </Grid>
   
